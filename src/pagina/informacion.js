267f895b4b3289dd566598f7c18a545c647dec7c
Noreidys Ramos Muentes <66332929+noreidysRM@users.noreply.github.com>
2020-10-14 14:36:18 -0500

    arreglo de la pantalla Información

--- src/pagina/informacion.js
+++ src/pagina/informacion.js
@@ -3,11 +3,13 @@ import React from "react";
 class Informacion extends React.Component {
   render() {
     return (
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm-4 col-md-4 col-lg-4">
-            <div className="text-center">
-              <h1>INFORMACION</h1>
+      <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
+    <h1>INFORMACION</h1>
               <button type="button" class="btn btn-secondary btn-lg">
                 <a className="nav-link active text-dark" href="ConoceTuCuerpo">
                   conoce tu cuerpo{" "}
@@ -45,10 +47,13 @@ class Informacion extends React.Component {
                 className="rounded"
                 alt="300px"
               />
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
+      
+    </div>
+  </div>
+</div>
+      
     );
   }
 }
